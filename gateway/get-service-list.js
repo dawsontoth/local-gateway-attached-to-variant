9ae34936bbc36fc3ab1afc0e9a0348f27ab1d7ce
kschrade <kyleschrade2@gmail.com>
2022-07-08 16:20:40 -0400

    moving to yaml config file

--- gateway/get-service-list.js
+++ gateway/get-service-list.js
@@ -1,5 +1,17 @@
-import { config } from '../config.js';
 import { exec } from 'child_process';
+import yaml from 'js-yaml';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = yaml.load(
+  fs.readFileSync(path.resolve(__dirname, '../settings.yaml'), {
+    encoding: 'utf-8',
+  })
+);
 
 function breakUpOutput(output) {
   // breaks the output from a long string into a string per line
